88ec719ecead93fa709b3494dbdd9bbb8fb6a6df
Nilay45 <nilay.15samanta@gmail.com>
2025-01-24 20:17:28 +0530

    cors changes

--- app.js
+++ app.js
@@ -15,19 +15,14 @@ config({
 // Using middlewares
 app.use(express.json());
 app.use(cookieParser());
-// app.use(cors({
-//     origin: [process.env.FRONTEND_URL],
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true
-//   })
-// );/
 app.use(cors({
-    origin: "*",
+    origin: [process.env.FRONTEND_URL || "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  }));
-  
+    credentials: true
+  })
+);
 
+  
 // Using Routes
 app.use( "/api/v1/users", userRouter);
 app.use( "/api/v1/task", taskRouter);
